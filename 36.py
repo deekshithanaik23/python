dcbe90bb9cc2bf73212b07634bf5b78d28301224
Deekshitha N Naik <deekshithanaik.sangamone@gmail.com>
2025-01-08 21:28:27 +0530

    Almost done but have to add two features

--- 36.py
+++ 36.py
@@ -1,4 +1,5 @@
 import random
+import sys
 f1=open("dictionary.txt","r")
 list1=f1.readlines()
 list2=[]
@@ -12,14 +13,24 @@ for i in range(0,len1,1):
     s1=list1[i].strip()
     list2.append(s1)
 
-randnum=random.randint(0,len1-1)
-compchoice1=list2[randnum]
-print("Computer:",compchoice1)
-userchoice1=input("User:")
-len1=len(userchoice1)
-userlastchar=userchoice1[len1-1:len1]
-temp1=list(filter(lambda i:i.startswith(userlastchar),list2))
-len1=len(temp1)
-randnum=random.randint(0,len1-1)
-compchoice1=temp1[randnum]
-print("Computer:",compchoice1)
+for i in range(0,10,1):
+    if i==0:
+        compchoice1=random.choice(list2)
+        print("Computer:",compchoice1,end=", ")
+    else:
+        temp1=list(filter(lambda i:i.startswith(userlastchar),list2))
+        compchoice1=random.choice(temp1)
+        print("Computer:",compchoice1,end=", ")
+
+    complastchar=compchoice1[-1]
+    temp1=list(filter(lambda i:i.startswith(complastchar),list2))
+    userchoice1=input("User:")
+    userlastchar=userchoice1[-1]
+
+    if userchoice1 in temp1:
+        pass
+    else:
+        print("Lost")
+        sys.exit()
+    
+
